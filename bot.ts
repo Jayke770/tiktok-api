f9b1e9840fbe62e44607d7134fa1e4db51fe0b9a
Jayke770 <jakevelasco40@gmail.com>
2022-12-22 19:49:10 +0800

    .

---
+++ bot.ts
@@ -0,0 +1,52 @@
+import tiktok from './tiktok'
+import { Bot, Context, session } from 'grammy'
+import { run } from "@grammyjs/runner"
+import { apiThrottler } from "@grammyjs/transformer-throttler"
+import { type Conversation, type ConversationFlavor, conversations, createConversation, } from "@grammyjs/conversations"
+type MyContext = Context & ConversationFlavor
+type MyConversation = Conversation<MyContext>
+const BOT_TOKEN = ""
+const bot = new Bot<MyContext>(BOT_TOKEN as string)
+const throttler = apiThrottler()
+bot.api.config.use(throttler)
+bot.use(session({ initial: () => ({}) }))
+bot.use(conversations())
+async function start(convo: MyConversation, ctx: MyContext) {
+    await ctx.reply("Enter TikTok Username:")
+    const { message } = await convo.wait()
+    await ctx.reply("Checking Tiktok Username...")
+    const { status, data } = await tiktok.fethUser(message?.text)
+    if (status) {
+        let text = '<b>ℹ️ User Tiktok Information</b>\n\n'
+        text += `<b>🔠 Name:</b> ${data.name}\n`
+        text += `<b>👤 Username:</b> ${data.username}\n`
+        text += `<b>📝 Bio:</b> ${data.bio}\n`
+        text += `<b>🔢 Followers:</b> ${data.followers?.toLocaleString()}\n`
+        text += `<b>🔢 Following:</b> ${data.following?.toLocaleString()}\n`
+        text += `<b>👍 Likes</b> ${data.likes?.toLocaleString()}\n`
+        await ctx.replyWithPhoto(data?.profile as string, {
+            caption: text,
+            parse_mode: "HTML",
+            reply_markup: {
+                inline_keyboard: [[
+                    { text: "🔗 View on TikTok", web_app: { url: data.link } }
+                ]]
+            }
+        })
+        return
+    } else {
+        await ctx.reply("Tiktok Information Not Found")
+        return
+    }
+}
+bot.use(createConversation(start))
+bot.command("start", async (ctx) => await ctx.conversation.enter("start"))
+const init = async () => {
+    await bot.init()
+    await bot.api.setMyCommands([
+        { command: 'start', description: "Start Bot" }
+    ])
+    console.log(`> Bot Started ${bot.botInfo.first_name}`)
+}
+init()
+run(bot)
--- tiktok.ts
+++ tiktok.ts
@@ -8,36 +8,45 @@ type UserData = {
     likes?: number,
     username?: string | null,
     bio?: string,
-    profile?: string
+    profile?: string,
+    link: string
 }
 const api = {
-    fethUser: async (username: string): Promise<UserData> => {
-        let data: UserData = { followers: 0, following: 0, likes: 0 }
+    fethUser: async (username?: string): Promise<{ status: boolean, data: UserData }> => {
+        let data: UserData = {}
         const browser = await puppeteer.launch()
         const page = await browser.newPage()
         await page.goto(`${tiktokurl}${username}`)
-        //name
-        const name = await page.$("[data-e2e='user-subtitle']")
-        data.name = await (await name.getProperty("textContent")).jsonValue()
-        //following
-        const following = await page.$("[data-e2e='following-count']")
-        data.following = api.convertNumberAbbreviation(await (await following.getProperty("textContent")).jsonValue())
-        //followers
-        const followers = await page.$("[data-e2e='followers-count']")
-        data.followers = api.convertNumberAbbreviation(await (await followers.getProperty("textContent")).jsonValue())
-        //likes
-        const likes = await page.$("[data-e2e='likes-count']")
-        data.likes = api.convertNumberAbbreviation(await (await likes.getProperty("textContent")).jsonValue())
-        //bio 
-        const bio = await page.$("[data-e2e='user-bio']")
-        data.bio = await (await bio.getProperty("textContent")).jsonValue()
-        //profile 
-        data.profile = await page.$eval(".tiktok-1zpj2q-ImgAvatar", e => e.getAttribute('src'))
-        await browser.close()
-        return data
-    },
-    followUSer: async (username: string) => {
-
+        const body = await page.$("body")
+        const bodyText = await (await body?.getProperty("innerText")).jsonValue()
+        if (bodyText.includes("Couldn't find this account")) {
+            await browser.close()
+            return { status: false, data: data }
+        } else {
+            //link 
+            data.link = `${tiktokurl}${username}`
+            //username 
+            data.username = username
+            //name
+            const name = await page.$("[data-e2e='user-subtitle']")
+            data.name = await (await name.getProperty("textContent")).jsonValue()
+            //following
+            const following = await page.$("[data-e2e='following-count']")
+            data.following = api.convertNumberAbbreviation(await (await following.getProperty("textContent")).jsonValue())
+            //followers
+            const followers = await page.$("[data-e2e='followers-count']")
+            data.followers = api.convertNumberAbbreviation(await (await followers.getProperty("textContent")).jsonValue())
+            //likes
+            const likes = await page.$("[data-e2e='likes-count']")
+            data.likes = api.convertNumberAbbreviation(await (await likes.getProperty("textContent")).jsonValue())
+            //bio 
+            const bio = await page.$("[data-e2e='user-bio']")
+            data.bio = await (await bio.getProperty("textContent")).jsonValue()
+            //profile 
+            data.profile = await page.$eval("#app > div.tiktok-ywuvyb-DivBodyContainer.e1irlpdw0 > div.tiktok-w4ewjk-DivShareLayoutV2.enm41490 > div > div.tiktok-1g04lal-DivShareLayoutHeader-StyledDivShareLayoutHeaderV2.enm41492 > div.tiktok-1gk89rh-DivShareInfo.ekmpd5l2 > div.tiktok-uha12h-DivContainer.e1vl87hj1 > span > img", e => e.getAttribute('src'))
+            await browser.close()
+            return { status: true, data: data }
+        }
     },
     convertNumberAbbreviation: (str: string) => {
         const regex = /^([\d.]+)([a-z]+)$/i;
